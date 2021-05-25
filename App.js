4399114606c80172f15fc7a37cf1c95eebf98997
Bhorn Ironside <tromesh6143@gmail.com>
2021-05-25 11:12:03 -0400

    added dynamic font and enablescreens for performance and low memory usage

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import { enableScreens } from 'react-native-screens';
 
 import productReducer from './store/reducers/product';
 import ShopNavigator from './navigation/ShopNavigator';
+
+enableScreens();
 
 const rootReducer = combineReducers({
   products: productReducer,
@@ -11,7 +16,26 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+const fetchFont = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFont}
+        onFinish={() => setDataLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
