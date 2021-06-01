cd5848ed40b9d2a496a9813fc9f28d01b809f959
Bhorn Ironside <tromesh6143@gmail.com>
2021-06-01 15:20:42 -0400

    added signup/login screen with signup auth flow

---
+++ store/actions/auth.js
@@ -0,0 +1,31 @@
+export const SIGN_UP = 'SIGN_UP';
+
+export const signUp = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB3DMaiIwm9P30h6Au9WBs5Z6IeTOSiEaM',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+
+    dispatch({
+      type: SIGN_UP,
+    });
+  };
+};
